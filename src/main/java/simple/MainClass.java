6581f053305feb33176e64a00936456d380cce91
Shubham Sahu <shubham.s@aitglobalinc.com>
2018-08-08 16:30:16 +0530

    worked on Second Commit

--- src/main/java/simple/MainClass.java
+++ src/main/java/simple/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("FirstCommit");
 		// TODO Auto-generated method stub
+		System.out.println("Second Commit");
 
 	}
 
