e20844dad1b0e2930e2cd22a6decf6f10106cbd7
Shubham Sahu <shubham.s@aitglobalinc.com>
2018-10-30 18:54:17 +0530

    Test for Jenkins

--- src/main/java/simple/MainClass.java
+++ src/main/java/simple/MainClass.java
@@ -3,17 +3,6 @@ package simple;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("FirstCommit");
-		// TODO Auto-generated method stub
-		System.out.println("Second Commit");
-		System.out.println("23August2018");
-System.out.println("Gitbash Commit testing");
-System.out.println("Gitbash Commit testing2");
-System.out.println("Gitbash Commit testing3");
-System.out.println("Gitbash Commit testing4");
-System.out.println("Gitbash Commit testing4");
-System.out.println("Gitbash Commit testing5");
-System.out.println("Gitbash Commit testing6");
+		System.out.println("Inside main method Maven_Java");
 	}
-
 }
