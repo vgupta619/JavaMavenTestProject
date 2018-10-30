544b094b28385272e7a153f0785a3bbff22a889d
Shubham Sahu <shubham.s@aitglobalinc.com>
2018-10-30 18:56:44 +0530

    Jenkins Test2

--- src/main/java/simple/MainClass.java
+++ src/main/java/simple/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Inside main method Maven_Java");
+		System.out.println("Inside main method Maven_Java 2nd phase");
 	}
 }
