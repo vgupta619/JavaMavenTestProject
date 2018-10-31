2a696fb88e1a4c99445b78c19f3b6107388cafcb
Shubham Sahu <shubham.s@aitglobalinc.com>
2018-10-31 13:21:11 +0530

    testtttt

--- src/main/java/simple/MainClass.java
+++ src/main/java/simple/MainClass.java
@@ -3,7 +3,6 @@ package simple;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Inside main method Maven_Java");
-		System.out.println("test2");
+		System.out.println("Inside main method Maven_Java2");
 	}
 }
